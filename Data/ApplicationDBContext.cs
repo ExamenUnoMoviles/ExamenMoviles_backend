db4145a6ec2255b15b7c03f01a3d54df2bf2d5db
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-05-06 11:36:11 -0600

    add migrations and course structure

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExamenMoviles_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamenMoviles_backend.Data
+{
+  public class ApplicationDBContext : DbContext
+  {
+    public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+    public DbSet<Course> Courses { get; set; }
+  }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,14 @@
+
+
+namespace ExamenMoviles_backend.Models
+{
+  public class Course
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string imageUrl { get; set; }
+    public string Schedule { get; set; }
+    public string? Professor { get; set; }
+  }
+}
